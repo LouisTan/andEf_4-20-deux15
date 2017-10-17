3fa2dc9f2553485c8e9c757d276be38be8e2c9fa
Lester <louis.tant@gmail.com>
2017-10-17 00:36:09 -0400

    Relation de symetrie infructueuse..

--- TP2_Louis/base.pl
+++ TP2_Louis/base.pl
@@ -11,19 +11,62 @@ amis(jean,pierre).
 amis(jean,paul). 
 amis(pierre,paul).
 amis(paul,jacques).
+
 % 3.5
-amis_2(X,Z) :- amis(X,Y) , amis(Y,Z).
+amis_2(X,Z) :- amis(X,Y) , amis(Y,Z) , X=Z.
+
 % 3.7
-homme(Hugo).
-homme(Loic).
-homme(Gabriel).
-homme(Maxime).
-homme(Mathieu).
-homme(Alexis).
-femme(Catherine).
-femme(Justine).
-femme(Lea).
-femme(Alice).
-femme(Rose).
-femme(Emma).
-parent(Hugo,Lea;Gabriel).
+homme(hugo).
+homme(loic).
+homme(gabriel).
+homme(maxime).
+homme(mathieu).
+homme(alexis).
+
+femme(catherine).
+femme(justine).
+femme(lea).
+femme(alice).
+femme(rose).
+femme(emma).
+
+marie(hugo,catherine).
+marie(loic,justine).
+marie(gabriel,alice).
+
+% Symetrie = boucle infinie!
+marie(X,Y) :- marie(X,Y);marie(Y,X).
+	
+parent(hugo,gabriel).
+parent(hugo,lea).
+parent(gabriel,alexis).
+parent(gabriel,rose).
+parent(gabriel,emma).
+parent(loic,alice).
+parent(loic,maxime).
+parent(loic,mathieu).
+
+enfant(X,Y) :- parent(Y,X).
+
+% Fait planter!
+parent(X,Y) :-
+	marie(X,Z),
+	parent(Z,Y).
+
+grand_parent(X,Y) :-
+	parent(Z,X),
+	parent(Y,Z).
+
+pere(X,Y) :-
+	parent(X,Y),
+	homme(X).
+
+mere(X,Y) :-
+	parent(X,Y),
+	homme(X).
+
+fils(X,Y) :-
+	%enfant(X,Y),
+	parent(Y,X),
+	homme(X).
+	
