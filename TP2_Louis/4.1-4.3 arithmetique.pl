bd180ccc3b0d686ee14e0dc40cc25226290cbb3c
Lester <louis.tant@gmail.com>
2017-10-17 15:23:20 -0400

    Supression base.pl

--- TP2_Louis/4.1-4.3 arithmetique.pl
+++ TP2_Louis/4.1-4.3 arithmetique.pl
@@ -1,12 +1,20 @@
+clc :- nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,
+	   nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,
+	   nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,
+	   nl,nl,nl,nl,nl,nl,nl,nl,nl,nl.
+
 % 3.1
 animal(chien). 
 animal(chat). 
+
 prenom(paul). 
 prenom(pierre). 
 prenom(jean). 
+
 possede(jean,chat). 
 possede(pierre,chien). 
 possede(pierre,cheval). 
+
 amis(pierre,jean). 
 amis(jean,pierre). 
 amis(jean,paul). 
@@ -14,4 +22,32 @@ amis(pierre,paul).
 amis(paul,jacques).
 
 % 3.5 - 3.6
-amis_2(X,Z) :- amis(X,Y) , amis(Y,Z) , X=Z.
+amis_2(X,Z) :- 
+	amis(X,Y),
+	amis(Y,Z),
+	X=Z.
+
+% 4.1
+amis(X) :- amis(X,_).
+
+% 4.3
+% 1
+sum(X,Y,R) :- R is X+Y.
+
+% 2
+max2(X,Y,M) :-
+	X < Y, M is Y;
+	X > Y, M is X;
+	X==Y, M is -1, 
+	write('Les valeurs sont identiques').
+	
+max3(X,Y,Z,M) :-
+	X == Y, max2(X,Z,N), M is N;
+	X == Z, max2(X,Y,N), M is N;
+	Y == Z, max2(X,Y,N), M is N;
+	max2(X,Y,N), max2(Z,N,T), M is T;
+	X == Y, X == Z, M is -1, 
+	write('Les valeurs sont identiques').
+
+% 3 d(F,X,G) :-
+	
