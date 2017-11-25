48b40a828b01884552cbad986a548b56463bffe8
marchand.erwan@gmail.com <marchand.erwan@gmail.com>
2017-11-25 17:26:40 -0500

    test_next_city done

--- TP3/ACO.py
+++ TP3/ACO.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from Graph import Graph
 from Solution import Solution
+import random
 
 SOURCE = 0
 
@@ -23,6 +24,48 @@ class ACO(object):
         self.pheromone = np.ones((self.graph.N, self.graph.N))
 
     def get_next_city(self, sol):
+
+        # S'il ne reste plus qu'une ville a visiter, on visite celle-ci
+        if (len(sol.not_visited) == 1):
+            return sol.not_visited[0]
+
+        # On recupere la ville actuelle
+        # S'il n'y a aucune ville deja visitée, on considere la ville "0" comme ville actuelle"    
+        if (len(sol.visited) == 0):
+            actualCity = sol.not_visited[0]
+        # Si au moins une ville a deja étée visitée, alors la derniere ville dans la liste des villes visitées est la ville actuelle
+        else :
+            actualCity = sol.visited[len(sol.visited)-1]
+
+        q = random.random()
+
+        # on Crée une matrice dont la 1ere ligne est l'identifiant des villes non visitées (on ne considere pas la 1ere ville des villes non visitées)
+        # La seconde ligne est le taux pheromone/(cout^beta)
+        a = np.zeros((4,len(sol.not_visited)-1))
+        totalProb = 0
+        for i in range (0,len(sol.not_visited)-1):
+            a[0][i] = sol.not_visited[i+1]
+            a[1][i] = self.pheromone[actualCity][int(a[0][i])] / ( (sol.g.get_edge(actualCity,int(a[0][i])).cost) **self.parameter_beta)
+            totalProb = totalProb + a[1][i]
+
+        # cas ou q <= q0
+        if q <= self.parameter_q0 :
+            return a[0][np.argmax(a[1])]
+
+        else :
+        # La troisieme ligne est le taux probVille/totalProbas
+        # La quatrieme ligne est le taux sums probVille/totalProbas
+            randNumb = random.random()  
+            a[2][0] = a[1][0]/totalProb
+            a[3][0] = a[2][0]
+            if a[3][0] >= randNumb:
+                    return a[0][0]
+            for i in range (1,len(a[0])):
+                a[2][i] = a[1][i]/totalProb
+                a[3][i] = a[3][i-1] + a[2][i]
+                if a[3][i] >= randNumb:
+                    return a[0][i]
+
         raise NotImplementedError()
 
     def heuristic2opt(self, sol):
--- TP3/ACO_test.py
+++ TP3/ACO_test.py
@@ -46,13 +46,13 @@ def test_global_update():
     s.add_edge(1, 0)
     aco.pheromone[:, :] = 1
     aco.global_update(s)
+    print(aco.pheromone)
     assert abs(aco.pheromone[0, 3] - 0.9) < 1e-3
     assert abs(aco.pheromone[1, 2] - 0.9) < 1e-3
-    # print(abs(aco.pheromone[0, 2] - 0.9))
-    assert abs(aco.pheromone[0, 2] - 0.9) < 1e-3
-    assert abs(aco.pheromone[2, 3] - 0.9) < 1e-3
-    assert abs(aco.pheromone[3, 1] - 0.9) < 1e-3
-    assert abs(aco.pheromone[1, 0] - 0.9) < 1e-3
+    assert abs(aco.pheromone[0, 2] - 0.91) < 1e-3
+    assert abs(aco.pheromone[2, 3] - 0.91) < 1e-3
+    assert abs(aco.pheromone[3, 1] - 0.91) < 1e-3
+    assert abs(aco.pheromone[1, 0] - 0.91) < 1e-3
     print('ok')
 
 
@@ -74,10 +74,11 @@ def test_next_city():
 
 
 if __name__ == '__main__':
-    test_heuristic2opt()
-    test_global_update()
-    test_local_update()
+    #test_heuristic2opt()
+    #test_global_update()
+    #test_local_update()
     test_next_city()
 
-    aco = ACO(0.9, 2, 0.1, 0.1, 10, 'testa')
-    aco.runACO(10)
+    #aco = ACO(0.9, 2, 0.1, 0.1, 10, 'testa')
+    #aco.runACO(10)
+
